187316f8c05df702e0f24ea59a223ebbb47dcb7b
RexOmiv <omivrex@gmail.com>
2022-04-13 13:47:49 +0100

    added meta tag for view port

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,6 +57,7 @@ export default function Home():ReactElement {
       <Head>
         <title>V-Sells</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
       <main className={styles.main}>
