4d23e301bb0d8df01172988d40dc8fcc302e8291
RexOmiv <omivrex@gmail.com>
2022-06-04 12:32:57 +0100

    changed footer content for sammy

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,40 +4,44 @@ import Link from 'next/link'
 import { BsFacebook } from 'react-icons/bs';
 import { SiGmail } from 'react-icons/si';
 import { IoLogoWhatsapp, IoLogoTwitter, IoLogoLinkedin } from 'react-icons/io';
-
+import {AiFillInstagram} from "react-icons/ai"
 const Footer = ():ReactElement => {
     return ( 
       <footer className={styles.footer}>
-        <h1>Desinged By Rex Omiv</h1>
+        <h1>Designed By Sammy Savage</h1>
         <p>
-          I am a fullstack web and mobile developer.
           <br />
-          I currently works as a freelance developer, I&lsquo;am focused on building applications for my clients that will gives them the satisfaction they deserve.
+            I am a fullstack web designer and developer.
+            I currently work as a self employed businessman who owns a brand called Sammy Gadget&lsquo;s, l&lsquo;m focused on designing websites and apps for my clients that
+            will give them the satisfaction they deserve.
+            I am a fullstack web and mobile developer.
           <br/>
-          V-SELLS is one of my web applications, it is an E-commerce web application desinged to showcase my frontend skills.
+            V-SELLS is one of my web applications, it is an E-
+            commerce web application desinged to showcase
+            my frontend skills.
         </p>
         <section className={styles.contactInfo}>
-          <Link href={'mailto:omivrex@gmail.com'} passHref={true}>
+          <Link href={'mailto:Samsonm199@gmail.com'} passHref={true}>
             <span>
               <SiGmail size={'40%'} color={'#ff5722'}/>
             </span>
           </Link>
-          <Link href={'https://web.facebook.com/rex.omivii'} passHref={true}>
+          <Link href={'https://www.instagram.com/sammy_craze/'} passHref={true}>
             <span>
-              <BsFacebook size={'40%'} color={'#0164b3'}/>
+              <AiFillInstagram size={'40%'} color={'#e91e63'}/>
             </span>
           </Link>
-          <Link href={'https://wa.me/+2347084973294'} passHref={true}>
+          <Link href={'https://wa.me/+2349057848503'} passHref={true}>
             <span>
               <IoLogoWhatsapp size={'40%'} color={'#4caf50'}/>
             </span>
           </Link>
-          <Link href={'https://twitter.com/RexOmiv'} passHref={true}>
+          <Link href={'https://twitter.com/Sammie98271022'} passHref={true}>
             <span>
               <IoLogoTwitter size={'40%'} color={'#0164b3'}/>
             </span>
           </Link>
-          <Link href={'https://linkedin.com/in/Rex-Omiv'} passHref={true}>
+          <Link href={'https://linkedin.com/in/samson-moses-458ab619a'} passHref={true}>
             <span>
               <IoLogoLinkedin size={'40%'} color={'#0164b3'}/>
             </span>
